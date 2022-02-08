6a559b6a9aef32603652a2086956cdabc6a16e5c
advidsec <advidsec@gmail.com>
2022-02-08 10:08:46 +0100

    Create domain2country.sh

---
+++ domain2country.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+  echo "Use: ./domains2country.sh [domain]"
+  exit 1
+fi
+filename='domains.txt'
+while read d || [[ -n $d ]]; do
+  ip=$(dig +short $1$d|grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"|head -1)
+  if [ -n "$ip" ]; then
+    echo "$d ; $ip"
+    echo $ip >> domains.tmp
+  else
+    echo "$d => Fail resolving"
+  fi
+done < $filename
